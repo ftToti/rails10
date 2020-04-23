986dcfe4c5451d9a222f9a8b0758f7fdc68178e4
vagrant <vagrant@localhost.localdomain>
2020-04-23 05:29:39 +0000

    [Finish]3-10

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -35,12 +35,40 @@ class TodolistsController < ApplicationController
   end
 
 
+  def edit
+
+    @list = List.find(params[:id])
+
+  end
+
+
+  def update
+
+    list = List.find(params[:id])
+
+    list.update(list_params)
+
+    redirect_to todolist_path(list.id)
+
+  end
+
+  def destroy
+
+    list = List.find(params[:id])
+
+    list.destroy
+
+    redirect_to todolists_path
+
+  end
+
+
 
   private
 
   def list_params
 
-	    params.require(:list).permit(:title, :body)
+	    params.require(:list).permit(:title, :body, :image)
 
   end
 end
--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7ade487badabe4ba1a460f4221ca85bfbe182c9bfe142eae910c3bac4729aced1f765d11fc7aa5dc67370aeabd189b0279ad84b6cf375373d09b1010d16b2e49'
